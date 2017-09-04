7c6a66d220f83eeafc11bf190ab2fcf50584889c
Srisaiyeegharan Kidnapillai <100680347@student.swin.edu.au>
2017-09-04 14:45:17 +1000

    Modified Zaid's work

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,25 @@
 export function iniCall () {
-  var url = '../../IJasonBackend/api/required_devices.php'
+  var url = '/api/required_devices.php'
   return url
 }
 
 export function bookedDevicesCall () {
-  var url = '../../IJasonBackend/api/booked_devices.php'
+  var url = '/api/booked_devices.php'
   return url
 }
 
 export function collectCall () {
-  var url = '../../IJasonBackend/api/collect_work.php'
+  var url = '/api/collect_work.php'
   return url
 }
 
 export function collectStatusCall () {
-  var url = '../../IJasonBackend/api/collect_status.php'
+  var url = '/api/collect_status.php'
   return url
 }
 
 export function feedbackCall () {
-  var url = '../../IJasonBackend/api/assess_work.php'
+  var url = '/api/assess_work.php'
   return url
 }
 
